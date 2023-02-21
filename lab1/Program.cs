f588ddd4442fec71fa8a863ccbb25016dd604ab1
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-02-21 16:57:31 +0200

    implement function to read float numbers from console

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -2,8 +2,35 @@
 
 public static class Program
 {
+  static float GetInputFloat(char name)
+  {
+    float k;
+    Console.Write($"{name} = ");
+
+    if (!float.TryParse(Console.ReadLine(), out k))
+      {
+          Console.WriteLine("Not a valid float. Try again");
+          k = GetInputFloat(name);
+      }
+      else {
+          Console.WriteLine($"You set {name} = {k}");
+      }
+    
+    return k;
+  }
   static void Main(string[] args)
   {
-    Console.WriteLine("Hello, World!");
+    float a, b, c;
+    
+    if (args.Length == 0)
+    {
+      a = GetInputFloat('a');
+      b = GetInputFloat('b');
+      c = GetInputFloat('c');
+    }
+    else
+    {
+      
+    }
   }
 }
