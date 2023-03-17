e0075cad4fd8ec953d3430ab917f93873be7104c
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-03-17 18:28:24 +0200

    implement methods: insert, delete and deleteAll

--- lab2/ListOfChars.cs
+++ lab2/ListOfChars.cs
@@ -29,6 +29,14 @@ public class ListOfChars
         count++;
     }
 
+    private void CheckIndex(int index)
+    {
+        if (index < 0 || index > count - 1)
+        {
+            throw new ArgumentOutOfRangeException("index");
+        }
+    }
+
     private void CheckLength()
     {
         if (count == items.Length)
@@ -36,10 +44,44 @@ public class ListOfChars
             Array.Resize(ref items, items.Length * 2);
         }
     }
+
+    public void insert(char item, int index)
+    {
+        CheckIndex(index);
+        CheckLength();
+
+        for (int i = count; i > index; i--)
         {
+            items[i] = items[i - 1];
         }
 
+        items[index] = item;
+        count++;
+    }
+
+    public char delete(int index)
+    {
+        CheckIndex(index);
+        char deletedItem = items[index];
+        count--;
+
+        for (int i = index; i < count; i++)
         {
+            items[i] = items[i + 1];
         }
+
+        items[count] = ' ';
+        return deletedItem;
+    }
+
+    public void deleteAll(char item)
+    {
+        for (int i = count - 1; i >= 0; i--)
+        {
+            if (items[i] == item)
+            {
+                delete(i);
+            }
+        } 
     }
 }
