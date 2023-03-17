22df35a0394dacf4d2ac65243d7b0f48d3ff6c71
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-03-17 18:27:18 +0200

    implement methods: length and append

--- lab2/ListOfChars.cs
+++ lab2/ListOfChars.cs
@@ -3,20 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace lab2
+namespace lab2;
+public class ListOfChars
 {
-    public class ListOfChars
-    {
-        private char[] items;
+    private char[] items;
+    private int count;
 
-        public ListOfChars()
+    public ListOfChars()
+    {
+        items = new char[5];
+        count = 0;
+    }
+
+    {
+
+    public int length()
+    {
+        return count;
+    }
+
+    public void append(char item)
+    {
+        CheckLength();
+        items[count] = item;
+        count++;
+    }
+
+    private void CheckLength()
+    {
+        if (count == items.Length)
         {
-            items = new char[];
+            Array.Resize(ref items, items.Length * 2);
+        }
+    }
+        {
         }
 
-        public int length()
         {
-            return items.Length;
         }
     }
 }
