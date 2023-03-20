9170e464b3b50a31e32e3c2f2c41f1a459788b59
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-03-20 18:38:36 +0200

    add test for insert method

--- lab2_tests/UnitTests_DoublyLinkedList.cs
+++ lab2_tests/UnitTests_DoublyLinkedList.cs
@@ -19,4 +19,34 @@ public class DoublyLinkedListTests
         Assert.AreEqual('d', list.get(3));
     }
 
+    [Test]
+    public void Insert()
+    {
+        var list = new DoublyLinkedList();
+        list.append('a');
+        list.append('b');
+        list.append('c');
+        list.append('d');
+        list.append('e');
+
+        list.insert('T', 3);
+
+        Assert.AreEqual(6, list.length());
+        Assert.AreEqual('T', list.get(3));
+        Assert.AreEqual('c', list.get(2));
+        Assert.AreEqual('d', list.get(4));
+    }
+
+    [Test]
+    public void Insert_IncorrectIndex()
+    {
+        var list = new DoublyLinkedList();
+        list.append('a');
+        list.append('b');
+        list.append('c');
+        list.append('d');
+        list.append('e');
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => list.insert('T', -1));
+    }
 }
