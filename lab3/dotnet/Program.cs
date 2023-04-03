e9baef14ca79fe3a32a3398f620d08f539eaeeec
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-04-03 16:45:03 +0300

    add libraries to project and use it in code

--- lab3/dotnet/Program.cs
+++ lab3/dotnet/Program.cs
@@ -1,18 +1,31 @@
 ﻿using System;
+using Bogus;
+using Newtonsoft.Json;
 
 namespace MyNamespace;
 class Program
 {
     static void Main(string[] args)
     {
-        var person = new Student { FirstName = "Yaryna", LastName = "Bashchak", Group = "IM-11", Age = 19 };
+        var faker = new Faker();
+        var rand = new Random();
+
+        var person = new Person {
+            FirstName = faker.Name.FirstName(),
+            LastName = faker.Name.LastName(),
+            Gender = faker.Person.Gender.ToString(),
+            Age = rand.Next(100)
+        };
+
+        var json = JsonConvert.SerializeObject(person);
+        Console.WriteLine(json);
     }
 }
 
-class Student
+class Person
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
-    public string? Group { get; set; }
+    public string? Gender { get; set; }
     public int Age { get; set; }
 }
