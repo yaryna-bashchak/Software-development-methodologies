f7f32eb87611358d3df634ba5e1d3f232329119c
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-04-03 14:25:48 +0300

    initial commit for lab3 (dotnet)

---
+++ lab3/dotnet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyNamespace;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var person = new Student { FirstName = "Yaryna", LastName = "Bashchak", Group = "IM-11", Age = 19 };
+    }
+}
+
+class Student
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Group { get; set; }
+    public int Age { get; set; }
+}
